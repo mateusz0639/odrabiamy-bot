ac4a1e4cc14eb30ff552b50f5c4e2753459df943
mateusz0639 <75630160+mateusz0639@users.noreply.github.com>
2022-12-19 13:47:55 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export default {
     channels: [ // 703977916492546086
         ''
     ],
-    odrabiamyAuth: '' // Odrabiamy.pl API v2 authorization token
+    odrabiamyAuth: '' // MTA1NDM3NTkwNjg5NjAwMzE2Mw.G2tWa6.AwvSrLQLXXHnbxCUh0WVMKPYLbhF7PN5uH4LwE
 }
