8d91dbd360a45d63bfe2ae3f4c17994a5cd47137
mateusz0639 <75630160+mateusz0639@users.noreply.github.com>
2022-12-19 13:50:39 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,7 @@ export default {
         ''
     ],
     odrabiamyAuth: '' // MTA1NDM3NTkwNjg5NjAwMzE2Mw.G2tWa6.AwvSrLQLXXHnbxCUh0WVMKPYLbhF7PN5uH4LwE
+$ npm install
+$ npm run build
+$ node ./dist/main.js
 }
